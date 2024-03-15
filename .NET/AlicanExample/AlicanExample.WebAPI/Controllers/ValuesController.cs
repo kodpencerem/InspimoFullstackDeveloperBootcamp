f138ef22fb7d7c9c7cbbfe8fc15074210cc9c9a6
TanerSaydam <tanersaydam@gmail.com>
2024-03-15 21:02:08 +0300

    15.03.2024 Ders Arası

---
+++ .NET/AlicanExample/AlicanExample.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlicanExample.WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { Message = "API is working..." });
+    }
+}
