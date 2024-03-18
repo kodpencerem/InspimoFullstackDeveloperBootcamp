113c593f1cfa53985cdc3b984610b5a1dceb3b04
TanerSaydam <tanersaydam@gmail.com>
2024-03-18 22:59:54 +0300

    18.03.2024

--- JavaScript/personel.js
+++ JavaScript/personel.js
@@ -1,3 +1,5 @@
+let personels = [];
+
 function save(event){
     event.preventDefault();
     const firstNameInputElement = document.getElementById("firstName");
@@ -14,5 +16,63 @@ function save(event){
         salary: salaryInputElement.value
     }
 
-    console.log(data);
+    personels.push(data);
+    
+    setPersonelsToTable();
+
+    firstNameInputElement.value = "";
+    lastNameInputElement.value = "";
+    professionInputElement.value = "";
+    startDateInputElement.value = "2024-03-18"
+    salaryInputElement.value = "17002";
+
+    firstNameInputElement.focus();
+}
+
+function setPersonelsToTable(){
+    debugger
+    const tbodyElement = document.querySelector("tbody");
+
+    personels = personels.sort((a,b) => 
+                    a.firstName.localeCompare(b.firstName));
+
+    let value = "";
+    for(const index in personels){
+
+        const date = new Date(personels[index].startDate);
+        const newDate = 
+        `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+
+        const salary = 
+        formatSalary(personels[index].salary.replace(",","."));
+
+        value += `
+                <tr>
+                    <td>${+index + 1}</td>
+                    <td>${personels[index].firstName}</td>
+                    <td>${personels[index].lastName}</td>
+                    <td>${personels[index].profession}</td>
+                    <td>${newDate}</td>
+                    <td>${salary}</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-primary">Update</button>
+                        <button class="btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                </tr>
+        `
+    } 
+
+    tbodyElement.innerHTML = value;
+}
+
+function formatSalary(salaryString){
+    const salaryNumber = +salaryString;
+
+    const formatter = new Intl.NumberFormat('tr-TR', {
+        style: "currency",
+        currency: "TRY",
+        minimumFractionDigits: 2
+    });
+
+    return formatter.format(salaryNumber);
 }
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Inspimo Fullstack Developer Bootcamp Eğitimi
 
-## 18.03.2024 (Gelecek ders)
+## 20.03.2024 (Gelecek ders)
+- JavaScript
+
+## 18.03.2024
 - JavaScript
 
 ## 16.03.2024 
