03b4fa978fec932e348d06566aeef0eb508ca03b
TanerSaydam <tanersaydam@gmail.com>
2024-05-03 21:31:32 +0300

    03.05.2024 Ders arası

--- .NET/eCommerce/eCommerce.MVC/Controllers/HomeController.cs
+++ .NET/eCommerce/eCommerce.MVC/Controllers/HomeController.cs
@@ -1,14 +1,49 @@
+using Bogus;
+using eCommerce.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.MVC.Controllers;
 public class HomeController : Controller
 {
+    private List<Product> products = new();
+
+    public HomeController()
+    {
+        Faker faker = new();
+
+        Product product1 = new()
+        {
+            Name = faker.Commerce.ProductName(),
+            ImageUrl = "https://cdn.yemek.com/uploads/2023/06/domates-kac-kalori-shutter-4.jpg",
+            Description = faker.Commerce.ProductDescription(),
+            Price = faker.Commerce.Price(symbol: "TRY") //altgr+t harfi => türk lirasý sembolü atar
+        };
+
+        products.Add(product1);
+
+        faker = new();
+
+        Product product2 = new()
+        {
+            Name = faker.Commerce.ProductName(),
+            Description = faker.Commerce.ProductDescription(),
+            Price = faker.Commerce.Price(symbol: "TRY"),
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Kiwi_%28Actinidia_chinensis%29_1_Luc_Viatour.jpg/640px-Kiwi_%28Actinidia_chinensis%29_1_Luc_Viatour.jpg"
+        };
+
+        products.Add(product2);
+    }
     public IActionResult Index() //action - action methodlar
     {
-        return View();
+        return View(products);
     }    
 
     public IActionResult Contact()
+    {
+        return View();
+    }
+
+    public IActionResult ShoppingCart()
     {
         return View();
     }
---
+++ .NET/eCommerce/eCommerce.MVC/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace eCommerce.MVC.Models;
+
+public sealed class Product
+{
+    public string ImageUrl { get; set; } = string.Empty; //""
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Price { get; set; } = string.Empty;
+}
