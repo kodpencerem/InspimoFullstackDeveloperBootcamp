63fd726402883491192d24d9af903f941c08586e
TanerSaydam <tanersaydam@gmail.com>
2024-05-13 20:53:52 +0300

    13.05.2024 Ders arası

---
+++ .NET/HttpContext.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpContext.WebAPI.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public sealed class ValuesController : ControllerBase
+{
+    [HttpGet("{age}/{name}")]
+    public IActionResult Get(int age, string name) //route params
+    {
+        var httpContext = HttpContext;
+
+        return NoContent();
+    }
+}
