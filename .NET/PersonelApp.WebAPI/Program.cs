d328f6de6655d58d8a0ebe5c5423ab41ae86f130
TanerSaydam <tanersaydam@gmail.com>
2024-06-05 20:58:14 +0300

    05.06.2024 - Ders arası

--- .NET/PersonelApp.WebAPI/Program.cs
+++ .NET/PersonelApp.WebAPI/Program.cs
@@ -1,49 +1,13 @@
-using PersonelApp.WebAPI.Context;
-using PersonelApp.WebAPI.Filters;
-using PersonelApp.WebAPI.Repositories;
-using PersonelApp.WebAPI.Services;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.MSSqlServer;
+using PersonelApp.WebAPI.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors();
+builder.Services.AddUI();
 
-builder.Services.AddControllers();
+builder.Services.AddDependencyInjection();
 
-builder.Services.AddTransient<ApplicationDbContext>();
-builder.Services.AddTransient<IPersonelRepository, PersonelRepository>();
-builder.Services.AddTransient<IPersonelService, PersonelService>();
+builder.Host.AddLog(builder.Configuration);
 
-builder.Services.AddTransient<IUserRepository, UserRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddTransient<IAuthTokenRepository, AuthTokenRepository>();
-builder.Services.AddTransient<IAuthTokenService, AuthTokenService>();
-
-builder.Services.AddMemoryCache();
-
-builder.Services.AddSwaggerGen();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console(LogEventLevel.Information)
-    .WriteTo.File("./log.txt", LogEventLevel.Information, rollingInterval: RollingInterval.Month)
-    .WriteTo.MSSqlServer(
-    connectionString: builder.Configuration.GetConnectionString("SqlServer"),
-    sinkOptions: new MSSqlServerSinkOptions()
-    {
-        TableName = "Logs",
-        AutoCreateSqlTable = true
-    })
-    .CreateLogger();
-
-builder.Host.UseSerilog();
-
-builder.Services.AddExceptionHandler<MyExceptionHandler>().AddProblemDetails();
 
 var app = builder.Build();
 
@@ -55,6 +19,5 @@ app.UseCors(x => x.AllowAnyOrigin());
 app.MapControllers();
 
 app.UseExceptionHandler();
-//app.UseMiddleware<MyExceptionHandler>();
 
 app.Run();
